af02e37bbe7fe97b43ee24a5790171bdeef1e670
cowilliams_deloitte <cowilliams@deloitte.com>
2025-01-07 16:16:06 -0700

    created book form and working localstorage

--- book-management-system/src/app/book/book.component.ts
+++ book-management-system/src/app/book/book.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../models/book.model';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.css']
 })
-export class BookComponent {
+export class BookComponent implements OnInit {
 
+  newBookTitle = "";
+  newBookAuthor = "";
+  allBooks: Book[] = [];
+
+  ngOnInit():void {
+    let savedBooks = localStorage.getItem('books');
+    this.allBooks = savedBooks ? JSON.parse(savedBooks) : [];
+    
+  }
+
+  addBook() {
+    if(this.newBookTitle.trim().length && this.newBookAuthor.trim().length){
+      let newBook: Book = {
+        id: Date.now(),
+        title: this.newBookTitle,
+        author: this.newBookAuthor
+      }
+
+      this.allBooks.push(newBook);
+
+      localStorage.setItem('books', JSON.stringify(this.allBooks))
+    }
+  }
+
+  deleteBook(index: number) {
+    this.allBooks.splice(index, 1);
+
+    localStorage.setItem('books', JSON.stringify(this.allBooks));
+  }
 }
