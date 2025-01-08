1120be2f783dff1bc476e44557df07ee0e59a077
cowilliams_deloitte <cowilliams@deloitte.com>
2025-01-08 16:06:07 -0700

    working on hotel app

--- hotel-app/src/app/reservation/reservation.service.ts
+++ hotel-app/src/app/reservation/reservation.service.ts
@@ -1,9 +1,40 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { Reservation } from "../models/reservation";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ReservationService {
 
-  constructor() { }
+  constructor() {
+    let savedReservations = localStorage.getItem('reservations');
+    this.reservations = savedReservations ? JSON.parse(savedReservations) : [];
+  }
+  private reservations: Reservation[] = [];
+
+  getReservations(): Reservation[] {
+    console.log('reservations', this.reservations)
+    return this.reservations;
+  }
+
+  getReservation(id: string): Reservation | undefined {
+    return this.reservations.find((res) => res.id === id);
+  }
+
+  addReservation(reservation: Reservation): void {
+    this.reservations.push(reservation);
+    localStorage.setItem('reservations', JSON.stringify(this.reservations));
+  }
+
+  deleteReservation(id: string): void {
+    let index = this.reservations.findIndex((res) => res.id === id);
+    this.reservations.splice(index, 1);
+    localStorage.setItem('reservations', JSON.stringify(this.reservations));
+  }
+
+  updateReservation(updatedReservation: Reservation): void {
+    let index = this.reservations.findIndex((res) => res.id === updatedReservation.id);
+    this.reservations[index] = updatedReservation;
+    localStorage.setItem('reservations', JSON.stringify(this.reservations));
+  }
 }
