a8c38be70c3ac9f531ff1626ed45f50d74a69ff0
cowilliams_deloitte <cowilliams@deloitte.com>
2025-01-13 12:38:46 -0700

    added prettier eslint

--- amazing-animal-paintings/src/app/product/product/product.module.ts
+++ amazing-animal-paintings/src/app/product/product/product.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { environment } from "src/environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Product } from "src/app/models/product";
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
 })
-export class ProductModule { }
+export class ProductModule {
+  constructor(private http: HttpClient) {}
+
+  private apiUrl = environment.apiUrl + "/products";
+  getProducts(): Observable<Product[]>{
+    return this.http.get<Product[]>(this.apiUrl);
+  }
+  
+}
