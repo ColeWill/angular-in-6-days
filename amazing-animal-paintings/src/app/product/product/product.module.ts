959ee3325d47f01bf5e7790b7980a2ce676522da
cowilliams_deloitte <cowilliams@deloitte.com>
2025-01-15 14:48:41 -0700

    completed day 4

--- amazing-animal-paintings/src/app/product/product/product.module.ts
+++ amazing-animal-paintings/src/app/product/product/product.module.ts
@@ -7,9 +7,18 @@ import { MatCardModule } from "@angular/material/card";
 import { MatSnackBarModule } from "@angular/material/snack-bar";
 import { MatInputModule } from "@angular/material/input";
 import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSelectModule } from "@angular/material/select";
 
 @NgModule({
   declarations: [ProductListComponent],
-  imports: [CommonModule, FlexModule, MatCardModule, MatSnackBarModule, MatInputModule, MatFormFieldModule],
+  imports: [
+    CommonModule,
+    FlexModule,
+    MatCardModule,
+    MatSnackBarModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+  ],
 })
 export class ProductModule {}
